9a8e3e5d22ecbe8caafc8baf64a8bcb519b6bffd
rushingbrendan <brushing@outlook.com>
2019-03-14 13:58:05 -0400

    added comments and function headers

---
+++ a4-sql-chart-data-visualization/WindowsFormsApp1/WindowsFormsApp1/Foodmart.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Foodmart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ProductClass = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Products = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SalesCities = new System.Windows.Forms.ListBox();
+            this.updateChartButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(417, 82);
+            this.chart1.MaximumSize = new System.Drawing.Size(574, 364);
+            this.chart1.MinimumSize = new System.Drawing.Size(574, 364);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(574, 364);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // ProductClass
+            // 
+            this.ProductClass.FormattingEnabled = true;
+            this.ProductClass.Location = new System.Drawing.Point(44, 63);
+            this.ProductClass.Name = "ProductClass";
+            this.ProductClass.Size = new System.Drawing.Size(160, 134);
+            this.ProductClass.TabIndex = 1;
+            this.ProductClass.SelectedIndexChanged += new System.EventHandler(this.ProductClass_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Product Class";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Product";
+            // 
+            // Products
+            // 
+            this.Products.FormattingEnabled = true;
+            this.Products.Location = new System.Drawing.Point(44, 233);
+            this.Products.Name = "Products";
+            this.Products.Size = new System.Drawing.Size(160, 134);
+            this.Products.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sales City";
+            // 
+            // SalesCities
+            // 
+            this.SalesCities.FormattingEnabled = true;
+            this.SalesCities.Location = new System.Drawing.Point(44, 399);
+            this.SalesCities.Name = "SalesCities";
+            this.SalesCities.Size = new System.Drawing.Size(160, 121);
+            this.SalesCities.TabIndex = 3;
+            // 
+            // updateChartButton
+            // 
+            this.updateChartButton.Location = new System.Drawing.Point(242, 478);
+            this.updateChartButton.Name = "updateChartButton";
+            this.updateChartButton.Size = new System.Drawing.Size(154, 42);
+            this.updateChartButton.TabIndex = 7;
+            this.updateChartButton.Text = "Update Chart";
+            this.updateChartButton.UseVisualStyleBackColor = true;
+            this.updateChartButton.Click += new System.EventHandler(this.updateChartButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(256, 102);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(103, 307);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear All Selections";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Foodmart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 574);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.updateChartButton);
+            this.Controls.Add(this.SalesCities);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Products);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProductClass);
+            this.Controls.Add(this.chart1);
+            this.Name = "Foodmart";
+            this.Text = "Advanced SQL Foodmart Chart - Brendan Rushing";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox ProductClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Products;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox SalesCities;
+        private System.Windows.Forms.Button updateChartButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ a4-sql-chart-data-visualization/WindowsFormsApp1/WindowsFormsApp1/Foodmart.cs
@@ -0,0 +1,376 @@
+﻿/*  Course:         PROG3070 - Advanced SQL
+ *  Assignment:     SQL Database data visualization in chart
+ *  Programmer:     Brendan Rushing 6020895
+ *  Date:           3/14/2019
+ *  
+ *  Description:
+ *      Display Foodmart data from SQL Server database in C# Windows Form Chart.
+ *      
+        Requirements
+        You will write a C# program (Forms or ASP.NET) that uses MS Chart to display a graph. Here are the
+        requirements:
+        1. You will use the Foodmart database to display the store sales data from the sales_fact_1998
+        table.
+        2. The graph should be a column chart.
+        3. The x-axis should be the Months of the year (as month names, not numbers).
+        4. Appropriate titles (x-axis, y-axis, Chart title, etc.) must be used.
+        5. You should be able to choose the chart by Product Class, Product and Sales City. Each of the
+        selection criteria can be “any one” or “all”. In other words, you can filter by any one Product
+        Class, or all Product Classes at a time. No need to select a specific set of Product Classes. These
+        choices are likely to be in drop-down list boxes and always visible on the form.
+        Note 1: The list of available Products is specific to the Product Classes chosen: when the Product
+        Class changes, the list of products might need to change.
+        Note 2: The Product Class, Product and Sales City must be displayed by name.
+        Note 3: Use the Product_Subcategory column in the Product_Class table as the text description.
+        6. Choose an appropriate method to refresh the graph. For example, you can either refresh each
+        time a new choice is made, or you can make your choices and have a button available to do the
+        refresh.
+        7. Make sure the connection string is specified in the app.config (or web.config) file. You do not
+        need any tool to change it. Assume it is editable using any editor. You can use the SQL Client
+        data objects (instead of OleDB).
+        8. The user interface is important. It should be easy to use and have no ambiguous information
+        presented.
+ * 
+ * 
+ * 
+ * */
+
+
+//includes
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Configuration;
+//eo includes
+
+namespace WindowsFormsApp1
+{
+    /// <summary>
+    /// Foodmart Chart Class
+    /// </summary>
+    public partial class Foodmart : Form
+    {
+        List<string> ProductClassItems = new List<string>();    //list of product classes
+        List<string> ProductItems = new List<string>();         //list of products
+        List<string> SalesCityItems = new List<string>();       //list of sales cities
+
+        double[]monthlySales = new double[12];                  //array of monthly sales
+
+        string connectionString = ConfigurationManager.AppSettings["databaseConnectionString"].ToString();  //connection string
+
+        string QueryString = "";        //query string
+        string ChartSeriesName = "Sales";   //chart name
+
+        bool QueryStringUpdated = false;
+        bool ApplicationFirstRun = true;
+        bool ChartExists = false;
+        
+
+       /// <summary>
+       /// Constructor
+       /// </summary>
+        public Foodmart()
+        {
+            InitializeComponent();
+
+            try
+            {
+                updateListBoxes();
+            }
+            catch (Exception)
+            {
+
+            }
+
+            //default all list boxes to no items selected
+            ProductClass.SetSelected(0, false);
+            Products.SetSelected(0, false);
+            SalesCities.SetSelected(0, false);
+
+            //add title
+            chart1.Titles.Add("Foodmart 1998 Sales");
+            ApplicationFirstRun = false;            
+        }
+
+        /// <summary>
+        /// Updates list boxes in form with item names from database
+        /// </summary>
+        private void updateListBoxes()
+        {
+            OleDbConnection conn = new OleDbConnection(connectionString);
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataReader rdr;
+            cmd.Connection = conn;
+
+            //get product classes
+            cmd.CommandText = "SELECT product_subcategory FROM product_class";
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                //add to product classes list
+                ProductClassItems.Add(rdr[0].ToString());
+            }
+            conn.Close();
+            ProductClass.DataSource = ProductClassItems;
+
+            //get product names
+            cmd.CommandText = "SELECT product_name FROM product";
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                //add to product names list
+                ProductItems.Add(rdr[0].ToString());
+            }
+            conn.Close();
+            Products.DataSource = ProductItems;
+
+            //get city store names
+            cmd.CommandText = "SELECT store_city FROM store";
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                //add to city store names list
+                SalesCityItems.Add(rdr[0].ToString());
+            }
+            conn.Close();
+            SalesCities.DataSource = SalesCityItems;
+            rdr.Close();
+        }
+
+
+        /// <summary>
+        /// Button handler to fill in chart with data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void updateChartButton_Click(object sender, EventArgs e)
+        {
+            //get store_id
+            int i = ProductClass.SelectedIndex; // RETURNS 1.
+
+            //build query string bases on selected items
+            buildQueryString();
+
+            //update monthly sales from database
+            updateMonthlySales();
+
+            //clear chart if exists
+            if (ChartExists == true)
+            {
+                ClearChartData();
+            }
+
+            //update chart
+            buildChartData();
+
+            ChartExists = true;
+        }
+
+        /// <summary>
+        /// Fill in all chart data
+        /// </summary>
+        private void buildChartData()
+        {
+            chart1.Series.Add(ChartSeriesName);            
+            chart1.ChartAreas[0].AxisX.Interval = 1;   //show all months
+            chart1.ChartAreas[0].AxisX.Title = "1998 Months";
+            chart1.ChartAreas[0].AxisY.Title = "Sales";
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "$ {}";
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+            //chart1.ChartAreas[0].AxisY.Maximum = Double.NaN;
+
+
+            //get monthly data from class
+            chart1.Series[ChartSeriesName].Points.AddXY("January", monthlySales[0]);
+            chart1.Series[ChartSeriesName].Points.AddXY("February", monthlySales[1]);
+            chart1.Series[ChartSeriesName].Points.AddXY("March", monthlySales[2]);
+            chart1.Series[ChartSeriesName].Points.AddXY("April", monthlySales[3]);
+            chart1.Series[ChartSeriesName].Points.AddXY("May", monthlySales[4]);
+            chart1.Series[ChartSeriesName].Points.AddXY("June", monthlySales[5]);
+            chart1.Series[ChartSeriesName].Points.AddXY("July", monthlySales[6]);
+            chart1.Series[ChartSeriesName].Points.AddXY("August", monthlySales[7]);
+            chart1.Series[ChartSeriesName].Points.AddXY("September", monthlySales[8]);
+            chart1.Series[ChartSeriesName].Points.AddXY("October", monthlySales[9]);
+            chart1.Series[ChartSeriesName].Points.AddXY("November", monthlySales[10]);
+            chart1.Series[ChartSeriesName].Points.AddXY("December", monthlySales[11]);
+
+        }
+
+        /// <summary>
+        /// Build query string based on list items selected
+        /// </summary>
+        private void buildQueryString()
+        {
+            bool itemAddedToString = false;
+            string currentQueryString = "SELECT SUM(store_sales) FROM sales_fact_1998 INNER JOIN time_by_day ON sales_fact_1998.time_id = time_by_day.time_id INNER JOIN store ON sales_fact_1998.store_id = store.store_id  INNER JOIN product ON sales_fact_1998.product_id = product.product_id";
+
+            ChartSeriesName = "All Sales";
+
+            //Product class is selected
+           if (ProductClass.SelectedIndex != -1)
+            {
+                currentQueryString += $" WHERE product.product_class_id = {ProductClass.SelectedIndex + 1}";
+                itemAddedToString = true;
+                QueryStringUpdated = true;                
+            }
+
+           //Product is selected
+           if (Products.SelectedIndex != -1)
+            {
+                if (itemAddedToString == false)
+                {
+                    currentQueryString += $" WHERE product.product_id = {Products.SelectedIndex + 1}";
+                }
+                else
+                {
+                    currentQueryString += $" AND product.product_id = {Products.SelectedIndex + 1}";
+                }
+
+                itemAddedToString = true;
+                QueryStringUpdated = true;
+            }
+
+            //Sales city is selected
+            if (SalesCities.SelectedIndex != -1)
+            {
+                if (itemAddedToString == false)
+                {
+                    currentQueryString += $" WHERE store.store_id = {SalesCities.SelectedIndex + 1}";
+                }
+                else
+                {
+                    currentQueryString += $" AND store.store_id = {SalesCities.SelectedIndex + 1}";
+                }
+                QueryStringUpdated = true;
+            }
+
+            //update class query string
+            QueryString = currentQueryString;
+        }
+
+        /// <summary>
+        /// Update monthly sales from database
+        /// </summary>
+        private void updateMonthlySales()
+        {
+            //get data for each month
+            for (int i = 1; i < 13; i++)
+            {
+                string currentQueryString = "";
+                int monthCounter = i - 1;
+                
+                if (QueryStringUpdated == false)
+                {
+                    currentQueryString = QueryString + $" WHERE time_by_day.month_of_year = {i}";
+                }
+                else
+                {
+                    currentQueryString = QueryString + $" AND time_by_day.month_of_year = {i}";
+                }
+
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                OleDbCommand cmd = new OleDbCommand();
+                OleDbDataReader rdr;
+                cmd.Connection = conn;
+                cmd.CommandText = currentQueryString;
+                conn.Open();
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    double currentTotal = 0;
+                    Double.TryParse(rdr[0].ToString(), out currentTotal);
+                    monthlySales[monthCounter] = currentTotal;
+                }
+                conn.Close();
+                rdr.Close();
+
+            }
+        }
+
+        /// <summary>
+        /// Clear selected list items
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ProductClass.SelectedIndex = -1;
+            SalesCities.SelectedIndex = -1;
+            Products.SelectedIndex = -1;
+
+            try
+            {
+                updateListBoxes();
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        /// <summary>
+        /// Update products based on selected product class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProductClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (ApplicationFirstRun == false)
+            {
+                //clear ProductItems
+                ProductItems.Clear();
+                Products.DataSource = null;
+                Products.Items.Clear();
+
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                OleDbCommand cmd = new OleDbCommand();
+                OleDbDataReader rdr;
+                cmd.Connection = conn;
+
+                cmd.CommandText = $"SELECT product_name FROM product WHERE product_class_id = {ProductClass.SelectedIndex + 1}";
+                conn.Open();
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    string temp = rdr[0].ToString();
+                    ProductItems.Add(rdr[0].ToString());
+                }
+                conn.Close();
+                Products.DataSource = ProductItems;
+                rdr.Close();
+
+                Products.SelectedIndex = -1;
+            }
+           
+        }
+
+        /// <summary>
+        /// Clear chart data
+        /// </summary>
+        private void ClearChartData()
+        {
+
+            chart1.Series.Clear();
+
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();                
+            }
+
+            ChartExists = false;
+        }
+    }
+}
